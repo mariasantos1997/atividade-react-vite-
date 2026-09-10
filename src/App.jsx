ddbbd468cf6fd1e5dac39cae9a39e9d5911fff81
Maria Eduarda Alves Dos Santos <marydogenshin@gmail.com>
2026-09-09 22:42:00 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+function App(){
+return (
+    <div className="bg-light min-vh-100 py-4">        
+    <div className="container">
+        <header>className="row mb-4"
+            <div className="col text-center">
+                <h1 className="display-5 fw-bold text-primary">
+                    Projeto React + Bootstrap Grid
+                </h1>
+                <p className="lead text-secundary">
+                    Exemplo de layout responsivo utilizando o sistema 
+                    de colunas do Bootstrap
+                </p>
+                </div>
+                </header> 
+    </div>
+    </div>
+);
+}
+
+export default App;
