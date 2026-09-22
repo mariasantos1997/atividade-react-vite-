04d4b93346483b4f0889be25dbf3676da5650753
mariasantos1997 <marydogenshin@gmail.com>
2026-09-22 20:50:03 -0300

    ajustes 22/09

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,12 @@ function App(){
 return (
     <div className="bg-light min-vh-100 py-4">        
     <div className="container">
-        <header>className="row mb-4"
+        <header className="row mb-4">
             <div className="col text-center">
                 <h1 className="display-5 fw-bold text-primary">
                     Projeto React + Bootstrap Grid
                 </h1>
-                <p className="lead text-secundary">
+                <p className="lead text-secondary">
                     Exemplo de layout responsivo utilizando o sistema 
                     de colunas do Bootstrap
                 </p>
